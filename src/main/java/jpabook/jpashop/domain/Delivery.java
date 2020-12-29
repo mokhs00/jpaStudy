2667a71f43e94b45e9420e07e5567dddbd253f8c
mokhs00 <mokhs00@naver.com>
2020-12-29 20:35:58 +0900

    Add Domains

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // EnumType.ORDINAL ( a, x, b) 숫자로 입력 추가 시 오류 남
+    private DeliveryStatus status;
+
+
+}
